aaec8598c710a7e22ba9d9e3a27373745e20d3d2
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-03-16 13:53:21 -0300

    Icones Favoritos Ativos

--- Angular - Memoteca/memoteca/src/app/components/pensamentos/pensamento/pensamento.component.ts
+++ Angular - Memoteca/memoteca/src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-pensamento',
@@ -12,9 +13,10 @@ export class PensamentoComponent implements OnInit {
     conteudo: 'I lova Angular',
     autoria: 'Duda',
     modelo: 'modelo3',
+    favorito: false,
   };
 
-  constructor() {}
+  constructor(private service: PensamentoService) {}
   ngOnInit(): void {}
 
   larguraPensamento(): string {
@@ -22,5 +24,15 @@ export class PensamentoComponent implements OnInit {
       return 'pensamento-g';
     }
     return 'pensamento-p';
+  }
+  mudarIconeFavorito(): string {
+    if (this.pensamento.favorito == false) {
+      return 'inativo';
+    }
+    return 'ativo';
+  }
+  atualizarFavoritos() {
+    this.service.mudarFavorito(this.pensamento).subscribe();
+    console.log(this.pensamento.favorito);
   }
 }
