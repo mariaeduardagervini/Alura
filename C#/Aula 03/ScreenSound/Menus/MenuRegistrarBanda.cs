2d170748db9041d82c3121b2e2b28d73a11504e1
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-06-27 12:26:10 -0300

    C#: Dominando Orientaçã a Objetos Aula 3/4

---
+++ C#/Aula 03/ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -0,0 +1,32 @@
+using OpenAI_API;
+using ScreenSound.Modelos;
+
+
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarBanda : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+       base.Executar(bandasRegistradas);
+        ExibirTituloDaOpcao("Registro das bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Banda banda = new Banda(nomeDaBanda);
+        bandasRegistradas.Add(nomeDaBanda, banda);
+
+        var client = new OpenAIAPI("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");
+
+        var chat = client.Chat.CreateConversation();
+
+        chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em 1 parágrafo. Adote um estilo informal.");
+
+        string resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        banda.Resumo = resposta;
+
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
